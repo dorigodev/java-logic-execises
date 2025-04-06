378c690aa43bd0684df7a0c33f1c68c3a9cad945
dorigo.dev <murillodorigo@gmail.com>
2025-04-05 21:05:45 -0300

    ✨ Feat: add psvm for exerciose01

--- src/exercises/exercise01.java
+++ src/exercises/exercise01.java
@@ -4,4 +4,7 @@ package exercises;
 * em seguida imprima na tela a soma entre A e B é mostre se a soma é menor que C.
 * */
 public class exercise01 {
+    public static void main(String[] args) {
+        
+    }
 }
