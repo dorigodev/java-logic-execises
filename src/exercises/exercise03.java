3feaf6fbb9df8008ebd0bc978608e7458b9becb4
dorigodev <murillodorigo@gmail.com>
2025-04-06 11:49:02 -0300

    ✨ Feat: add exercise 03 real answer

--- src/exercises/exercise03.java
+++ src/exercises/exercise03.java
@@ -1,25 +1,33 @@
 package exercises;
 /*
 * Faça um algoritmo que leia dois valores inteiros A e B,
-*  se os valores de A e B forem iguais, deverá somar os dois valores,
- * */
+* se os valores de A e B forem iguais, deverá somar os dois valores,
+* caso contrário devera multiplicar A por B.  Ao final de qualquer um dos cálculos deve-se atribuir o
+* resultado a uma variável C e
+ * imprimir seu valor na tela.
+*/
 
 
 import java.util.Scanner;
 
 public class exercise03 {
     public static void main(String[] args) {
-        System.out.println("Digit fist number: ");
         Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
+
+        System.out.println("Digit fist number: ");
+        int A = sc.nextInt();
 
         System.out.println("Digit second number: ");
-        int number2 = sc.nextInt();
-        if (number == number2) {
+        int B = sc.nextInt();
+
+        int C;
+        if (A == B) {
             System.out.println("Os dois valores são iguais e a soma deles é: ");
-            System.out.println(number + number2);
+            System.out.println(A + B);
         } else {
-            System.out.println("Os dois valores não são iguais");
+            C = A * B;
+            System.out.println("Os dois valores não são iguais e a multiplicacao deles é:");
+            System.out.println(C);
         }
 
     }
